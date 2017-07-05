2ae81012e0c521f9a2e1c317bced2f3771bff54a
Anand Singh <anand.singh@student.uva.nl>
2017-07-05 14:34:50 +0200

    Add Scala Project to Git

---
+++ Scala Project/src/Hello_Scala.scala
@@ -0,0 +1,9 @@
+/**
+  * Created by anand on 7/5/17.
+  */
+object Hello_Scala {
+  def main(args: Array[String]): Unit = {
+    println("Hello Scala")
+  }
+
+}
---
+++ Scala Project/src/Hellomundo.scala
@@ -0,0 +1,10 @@
+/**
+  * Created by anand on 7/5/17.
+  */
+object Hellomundo {
+
+  def main(args: Array[String]): Unit ={
+    println("Hello World!... I mean, Mundo")
+  }
+
+}
