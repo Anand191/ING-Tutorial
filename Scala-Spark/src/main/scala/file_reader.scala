10e73dd1ffd7d90021ee908dda58dd704723b12a
Anand Singh <anand.singh@student.uva.nl>
2017-08-01 14:57:34 +0200

    Running the app via a scala wrapper

--- Scala-Spark/src/main/scala/file_reader.scala
+++ Scala-Spark/src/main/scala/file_reader.scala
@@ -1,6 +1,23 @@
 /**
   * Created by anand on 7/12/17.
   */
+import org.apache.spark.{SparkConf, SparkContext}
+import org.apache.spark.sql.SparkSession
 object file_reader {
+  def main(args:Array[String]): Unit = {
+    val conf = new SparkConf()
+    conf.setAppName("File Reading Test")
+    conf.setMaster("local[*]")
+    val sc = new SparkContext(conf)
+    println(sc)
+
+    val spark = SparkSession
+      .builder()
+      .appName("Spark SQL basic example")
+      .config("spark.some.config.option", "some-value")
+      .getOrCreate()
+    val df = spark.read.format("com.databricks.spark.csv").option("header", "true").load("file:///home/anand/UvA/ING/Mechanical Turk/5/CPU50.csv")
+    df.show()
+  }
 
 }
