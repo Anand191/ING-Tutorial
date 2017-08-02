a4306ca2d707c1581e4572550c4073e21683ab32
Anand Singh <anand.singh@student.uva.nl>
2017-08-02 11:43:16 +0200

    paths according to Jenkins workspace

--- Scala-Spark/src/main/scala/run_python.scala
+++ Scala-Spark/src/main/scala/run_python.scala
@@ -2,7 +2,7 @@ import sys.process._
 
 object run_python {
   def main(args: Array[String]): Unit = {
-    val result = "JenkinsCI/bin/bokeh serve --show --port 5001 Fifth_Trial.py".!
+    val result = "$WORKSPACE/Scala-Spark/JenkinsCI/bin/bokeh serve --show --port 5001 $WORKSPACE/Scala-Spark/Fifth_Trial.py".!
     println(result)
   }
 
