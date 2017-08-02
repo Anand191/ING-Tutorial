ea50b42a74dfad92500be671ee1e01ef849d718c
Anand Singh <anand.singh@student.uva.nl>
2017-08-02 13:24:26 +0200

    tinkering with paths

--- Scala-Spark/src/main/scala/run_python.scala
+++ Scala-Spark/src/main/scala/run_python.scala
@@ -1,8 +1,7 @@
 import sys.process._
-
 object run_python {
   def main(args: Array[String]): Unit = {
-    val result = "$WORKSPACE/Scala-Spark/JenkinsCI/bin/bokeh serve --show --port 5001 $WORKSPACE/Scala-Spark/Fifth_Trial.py".!
+    val result = "/var/lib/jenkins/workspace/ING_Turk/Scala-Spark/JenkinsCI/bin/bokeh serve --show --port 5001 /var/lib/jenkins/workspace/ING_Turk/Scala-Spark/Fifth_Trial.py".!
     println(result)
   }
 
